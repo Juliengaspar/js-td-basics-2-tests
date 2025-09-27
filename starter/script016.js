8a3b9f89e45a49caa386eaadb3bb17d69930d6bf
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-27 14:12:48 +0200

    end-ex script016 operation ternaires(true or false) & switch avec et sans break

--- starter/script016.js
+++ starter/script016.js
@@ -6,24 +6,63 @@
 
 // 1. Écrivez une expression ternaire pour afficher "John boit des bières" ou "John boit des jus" selon qu'il est majeur ou pas.
 // 2. Testez en changeant l'âge de John (14 ans, 24 ans)
-
+const firstName = "John";
+let age = 15;
+if(age <18 ){
+    console.log(`${firstName} boit des jus`)
+}else {
+    console.log(`${firstName} boit des bières`)
+}
 // MEILLEURE PRATIQUE : utiliser l'expression ternaire pour affecter une valeur ou une autre valeur à une variable selon qu'une condition est remplie ou pas
 // 1. Affectez, selon qu'on est majeur ou pas, la valeur "bières" ou la valeur "jus" à la variable drink
+let drink;
+if(age <18 ){
+    drink = "jus";
+}else {
+    drink = "bières";
+}
 // 2. Afficher "John boit des …" (l'affichage s'adapte à l'âge de John, tester différents cas)
-
+console.log(`${firstName} boit des ${drink}`);
 // Faites la même chose en utilisant un if / else
-
+if(age <18 ){
+    console.log(`${firstName} boit des jus`)
+}else {
+    console.log(`${firstName} boit des bières`)
+}
 // 16 - 2. L'instruction Switch
+switch (true){
+    case age<18:
+        console.log(`${firstName} boit des jus`);
+        break;
+    case age>18:
+        console.log(`${firstName} boit des bières`);
+        break;
+    default:
+        console.log("valeur invalid")
+}
 
 // a) Switch avec des break
 // 1. Initialisez la variable job à la valeur "instituteur"
+const job = "etudiants";
 // 2. Utilisez un switch pour afficher :
 //   - si la profession est professeur ou instituteur, "John enseigne la programmation aux enfants",
 //   - si c'est chauffeur, "John conduit un taxi à Lisbonne",
 //   - si c'est designer, "John conçoit de beaux sites web" et,
 //   - dans tous les autres cas, "John fait autre chose"
 // 3. Testez les différents cas de figure en changeant la profession de John
-
+switch (job){
+    case "instituteur":
+        console.log(`${firstName} enseigne la programmation aux enfants`);
+        break;
+    case "chauffeur":
+        console.log(`${firstName} conduit un taxi à Lisbonne`);
+        break;
+        case "designer":
+        console.log(`${firstName} conçoit de beaux sites web`);
+        break;
+    default:
+        console.log("fait autre chose")
+}
 // 1. John a vielli : il a à présent 56 ans…
 // 2. Utilisez un switch pour affciher :
 //   - si l'âge est inférieur à 13, "John est un garçon",
@@ -32,7 +71,20 @@
 //   - et sinon "John est un homme".
 // 3. Testez les différents cas de figure en changeant l'âge de John
 // 4. Testez avec un âge de 7 ans en enlevant la 2e instruction break pour voir ce que cela a comme impact
-
+age = 7;
+switch (true){
+    case age<13:
+        console.log(`${firstName} est un garçon`);
+        break;
+    case age>=13 && age<20:
+        console.log(`${firstName} est un adolescent`);
+        break;
+    case age>=20 && age<30:
+        console.log(`${firstName} est un jeune homme`);
+        break;
+    default:
+        console.log("est homme")
+}
 // Autre application de l'instruction switch
 // 1. Demandez à l'utilisateur d'entrer l'information
 //   "Quel temps fait-il dehors ? Répondez par un des quatre mots suivants :
@@ -46,10 +98,49 @@
 //     — c.-à-d. qu'elle n'a entré aucun de ces qautre mots-là),
 //     affichez "Je n'ai pas compris !"
 
+const meteo = prompt("Quel temps fait-il dehors ? Répondez par un des quatre mots suivants :soleil, vent, pluie ou neige.");
+switch (meteo) {
+    case "soleil":
+        console.log("Sortez en t-shirt.");
+        break;
+    case "vent":
+        console.log("Sortez en pull.");
+        break;
+    case "pluie":
+        console.log("Sortez en blouson.");
+        break;
+    case "neige":
+        console.log("Restez au chaud à la maison.");
+        break;
+    default:
+        console.log("Je n'ai pas compris !");
+}
+
 // b) Switch sans break
 // L'instruction `break` fait sortir du bloc du switch. On ne souhaite pas toujours sortir, dans ce cas, on peut ne pas mettre de `break`
 // 1. Demandez à l'utilisateur d'entrer le numéro du jour de la semaine
 // 2. Affichez ensuite dans la console le message suivant : "Les jours suivants se sont déjà écoulés depuis le début de la semaine : …, …, …"
+const numberDays = parseInt(prompt("entrer le numéro du jour de la semaine [1, 7]"));
+console.log("Les jours suivants se sont déjà écoulés depuis le début de la semaine :");
+switch (numberDays){
+    case 7:
+        console.log("samedi");
+        case 6:
+        console.log("vendredi");
+        case 5:
+        console.log("jeudi");
+        case 4:
+        console.log("mercredi");
+        case 3:
+        console.log("mardi");
+        case 2:
+        console.log("lundi");
+        case 1:
+            console.log("Aucun jour ne s'est encore écoulé.");
+            break;
+            default:
+        console.log("numero invalid");
+}
 
 // Ici, toutes les instructions "console.log" entre la ligne case:… et l'instruction break seront exécutées, et on obtient donc une liste de jours. Notez bien qu'il faut quand même un break avant le default, sans quoi ce message d'erreur apparaîtra toujours à la fin de notre liste de jours… Notez qu'on aurait pu aussi écrire le default en premier, suivi d'un break puis la liste de tous les case.
 
