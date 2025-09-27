9c5f1b141ec90c010265754dacae4fd3628b55cb
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-27 10:47:08 +0200

    end-ex 3.2 avec if et else if

--- OCR-starter/chapitre3_exercice2.js
+++ OCR-starter/chapitre3_exercice2.js
@@ -7,6 +7,15 @@
    - "Le candidat est reçu" si la moyenne est entre 10 et 12.
    - "Le candidat est reçu avec mention" si la moyenne est supérieure ou égale à 12.
 */
+const candidateAverage = prompt("entrez une valeur entre 0 et 10")
+
+if (candidateAverage<10){
+    console.log("Le candidat est recalé");
+}else if ((candidateAverage>=10)&&(candidateAverage<12)){
+    console.log("Le candidat est reçu")
+}else {
+    console.log("Le candidat est reçu avec mention")
+}
 
 // Exemple : si la moyenne du candidat est de 11, le programme devrait afficher "Le candidat est reçu."
 
