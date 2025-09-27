8ac3e90b53df935d1884ca36583662adfe31c9d3
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-27 12:02:41 +0200

    end-ex script014 different maniers de faires des teste de maniers different pour different cas

--- starter/script014.js
+++ starter/script014.js
@@ -9,30 +9,85 @@
 
 // 1. Écrivez un test dont la condition est true
 // 2. Dans le bloc, affichez "Je suis dans un bloc d'instructions soumis à une condition qui est toujours vraie, je vais donc toujours m'afficher"
-
+const trueValeur = true;
+if (trueValeur === true){
+    console.log("Je suis dans un bloc d'instructions soumis à une condition qui est toujours vraie, je vais donc toujours m'afficher");
+}
 // Et l'inverse :
 // 1. Écrivez un test dont la condition est false
 // 2. Dans le bloc, affichez "Bon, je cause toujours, mais vu que je suis dans un bloc d'instructions soumis à une condition qui est toujours fausse, ce que je dis ne va jamais s'afficher"
-
+const test2 = false;
+if (test2 === true){
+    console.log("Bon, je cause toujours, mais vu que je suis dans un bloc d'instructions soumis à une condition qui est toujours fausse, ce que je dis ne va jamais s'afficher");
+}
 // 1. Demandez à l'utilisateur d'entrer un nombre et enregistrez ce nombre (ATTENTION, prenez bien soin qu'il s'agisse bien d'un nombre et pas d'une chaîne de caractères !) dans une constante nombre
 // 2. Si ce nombre est positif (et seulement dans ce cas), affichez "… est positif"
-
+const nombre = parseInt(prompt("entrez un nombre alatoires"))
+if (nombre>0){
+    console.log(`${nombre} est positif`)
+}
 // Écrivez un programme qui dit "Bonjour" à l'utilisateur et n'accepte de lui répondre que si celui-ci lui dit "Bonjour" à son tour. Dans ce cas, le programme lui répond "Toi, tu es poli, tu me dis bonjour, je veux bien te parler… Comment t'appelles-tu ?". Il enregistre alors l'entrée dans une variable nom et, seulement si l'utilisateur a bien entré quelque chose dans le champ (il prend la peine de vérifier avant), il lui répond (dans la console), "Je t'aime bien, …".
+const hello = (prompt("Bonjour"));
+if (hello === "Bonjour"){
+    console.log("Bonjour")
+    const nom = (prompt("Toi, tu es poli, tu me dis bonjour, je veux bien te parler… Comment t'appelles-tu ?"))
+    if (nom === "") {
+    } else {
+        console.log(`Je t'aime bien, ${nom}`)
+    }
+}else {
+    console.log("tu me dis même pas Boujour mal polis")
+}
 
 // 2. Exprimez une alternative : if / else
 
 // 1. Demandez à l'utilisateur d'entrer un nombre et enregistrez ce nombre dans une constante nombre
 // 2. Si ce nombre est positif, affichez "… est positif", sinon, affichez "… est négatif ou nul"
-
+const numberValues = parseInt(prompt("entrez un nombre alatoires"))
+if (numberValues>0){
+    console.log(`${numberValues} est positif`)
+}else {
+    console.log(`${numberValues} est négatif ou nul`)
+}
 // 1. Initialisez la variable firstName à  John et enregistrez son état civil ('single') dans une variable civilStatus
 // 2. Si l'état civil est 'married', affichez "John est marié !", sinon affichez "John va, on l\'espère, bientôt se marier :)"
 // 3. Changez l'état civil de John à ('married') et retestez
+const firstName = "john";
+const civilStatus = "single";
+if (civilStatus === "married"){
+    console.log(`${firstName} est ${civilStatus} !`)
+}else {
+    console.log(`${firstName} va, on l\'espère, bientôt se marier :)`)
 
+}
 // 1. Créez à présent une variable isMarried pour enregistrer l'état civil de John et, en imaginant qu'il est marié, initialisez-la à true
 // 2. Servez-vous à présent de cette variable pour afficher "John est marié !" ou "John va, on l\'espère, bientôt se marier :)" selon qu'il est marié ou pas
 // 3. Retestez votre code après avoir changé la valeur de isMarried à false
-
+const isMarried = true;
+if (isMarried === true){
+    console.log(`${firstName} est marié !`)
+}else {
+    console.log(`${firstName} va, on l'espère, bientôt se marier :)`)
+}
 // 1. Enregistrez la taille et le poids de John (78 kg, 1.69 m) et de Mark (92 kg, 1.95 m) dans 4 variables
 // 2. Calculez leur BMI et affichez, selon le cas, "Le BMI de Mark est plus élevé que celui de John" ou "Le BMI de John est plus élevé que celui de Mark"
+const tailleJohn = 1.69;
+const poidsJohn = 78;
+const tailleMark = 1.95;
+const poidsMark = 92;
+const bmiJohn = poidsJohn / (tailleJohn*tailleJohn);
+const bmiMark = poidsMark/ (tailleMark*tailleMark);
+if (bmiMark > bmiJohn){
+    console.log(`"Le BMI de Mark ${bmiMark} est plus élevé que celui de John ${bmiJohn}`);
+}else {
+    console.log(`"Le BMI de John ${bmiJohn} est plus élevé que celui de Mark ${bmiMark}`);
 
+}
 // Écrire un programme qui teste si un nombre entré par l’utilisateur est pair ou impair.
+const numberType = parseInt(prompt("entrez un nombre"));
+if (numberType%2 === 0){
+    console.log(`${numberType} est paire`);
+}else {
+    console.log(`${numberType} est impaire`);
+
+}
