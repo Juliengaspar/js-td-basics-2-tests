72f2a1f55c78e32b3a72eea642cdabeefd485882
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-27 11:28:56 +0200

    end-ex 3.3 avec calcule heur , minutes, secondes avec il & else

--- OCR-starter/chapitre3_exercice3.js
+++ OCR-starter/chapitre3_exercice3.js
@@ -10,3 +10,27 @@
 // Si l'heure actuelle était égale à 23, réinitialisez l'heure à 0 (minuit).
 //     Sinon, incrémentez l'heure actuelle.
 // Affichez la nouvelle heure, les minutes et les secondes dans la console.
+let hours = parseInt (prompt("entrez une valeurs entre [0, 24]"));
+let minutes = parseInt (prompt("entrez une valeurs entre [0, 60]"));
+let seconde = parseInt (prompt("entrez une valeurs entre [0, 60]"));
+if ((seconde>60) || (minutes>60) || (hours>24)){
+    console.log("score invalide")
+
+}else {
+    //seconde = seconde+1
+
+    if (seconde === 59) {
+        seconde = 0;
+    }
+    if (minutes === 59){
+        minutes = 0;
+    }
+    if (hours === 23){
+        hours = 0;
+    }
+    console.log(typeof hours);
+    console.log(typeof minutes);
+    console.log(typeof seconde);
+    console.log(`${hours} : ${minutes} : ${seconde}`);
+}
+
