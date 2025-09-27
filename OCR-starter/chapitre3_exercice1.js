dc5850ff41d0c780c5a538f1f989fbeb1475da83
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-27 10:39:20 +0200

    end-ex 3.1 avec les if , else if , else

--- OCR-starter/chapitre3_exercice1.js
+++ OCR-starter/chapitre3_exercice1.js
@@ -13,7 +13,64 @@
    - Si le jour actuel ne correspond à aucun des jours de la semaine, affichez dans la console "Erreur : jour non reconnu!"
 3. Testez si la variable qui contient le nom du jour suivant n'est pas vide, et dans ce cas, affichez dans la console le message "Demain, nous serons …" suivi du nom du jour suivant.
 */
+const currentdays = prompt("Quel jours nous sommes aujourd'ui ");
 
 // Exemple : si le jour actuel est "mardi", le programme devrait afficher "Demain, nous serons mercredi."
 
 // N'oubliez pas de tester votre programme avec divers jours de la semaine.
+/*
+avec le cas switch et case
+switch (currentdays) {
+    case "lundi":
+        console.log("alors le jour suivant est mardi.");
+        break;
+        case "mardi":
+        console.log("alors le jour suivant est mercredi.");
+        break;
+        case "mercredi":
+        console.log("alors le jour suivant est jeudi.");
+        break;
+        case "jeudi":
+        console.log("alors le jour suivant est vendredi.");
+        break;
+        case "vendredi":
+        console.log("alors le jour suivant est samedi.");
+        break;
+        case "samedi":
+        console.log("alors le jour suivant est dimanche.");
+        break;
+        case "dimanche":
+        console.log("alors le jour suivant est lundi.");
+        break;
+    default:
+        console.log("Erreur : jour non reconnu!");
+}
+
+ */
+//avec if et else if
+if (currentdays === "lundi") {
+    console.log("alors le jour suivant est mardi.");
+}
+else if (currentdays === "mardi") {
+    console.log("alors le jour suivant est mercredi.");
+
+}
+else if (currentdays === "mercredi") {
+    console.log("alors le jour suivant est jeudi.");
+
+}
+else if (currentdays === "jeudi") {
+    console.log("alors le jour suivant est vendredi.");
+}
+else if (currentdays === "vendredi") {
+    console.log("alors le jour suivant est samedi.");
+}
+else if (currentdays === "samedi") {
+    console.log("alors le jour suivant est dimanche.");
+}
+else if (currentdays === "dimanche") {
+    console.log("alors le jour suivant est lundi.");
+}
+else {
+    console.log("Erreur : jour non reconnu!");
+}
