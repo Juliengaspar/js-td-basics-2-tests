0353564057ccfbacb32ecc06075ab634290ceef6
Juliengaspar <julien.gaspar@student.hepl.be>
2025-09-28 10:45:10 +0200

    end-ex script018-CC2.js tester qui a le meilleurs moyennes de score avec des test imbriquers

--- starter/script018-CC2.js
+++ starter/script018-CC2.js
@@ -24,15 +24,58 @@ BONNE CHANCE 😀
 // sans bonus
 
 // 1. Calculez le score moyen de chaque équipe.
+let scoreMoyenJohn = (89+120+103)/3;
+let scoreMoyenMike = (116+94+123)/3;
 // 2. Décidez quelle équipe gagne en moyenne (celle qui a le plus haut score moyen)
 //    et imprimez l'équipe gagnante dans la console avec son score moyen.
+if (scoreMoyenJohn > scoreMoyenMike){
+    console.log(`john a le  plus haut score moyen ${scoreMoyenJohn} > ${scoreMoyenMike}`);
+}else {
+    console.log(`mike a le  plus haut score moyen ${scoreMoyenMike} > ${scoreMoyenJohn}`);
+
+}
 // 3. Changez ensuite les scores pour montrer différents gagnants.
 //    N'oubliez pas de prendre en compte qu'il pourrait y avoir un ex æquo (le même score moyen).
+scoreMoyenJohn = (124+156+111)/3;
+scoreMoyenMike = (100+123+159)/3;
+if (scoreMoyenJohn > scoreMoyenMike){
+    console.log(`john a le  plus haut score moyen ${scoreMoyenJohn} > ${scoreMoyenMike}`);
+}else {
+    console.log(`mike a le  plus haut score moyen ${scoreMoyenMike} > ${scoreMoyenJohn}`);
 
+}
 // avec bonus
 
 // 4. BONUS : Mary joue aussi au basket, et son équipe a marqué 97, 134 et 105 points.
+scoreMoyenJohn = (89+120+103)/3;
+scoreMoyenMike = (116+94+123)/3;
+let scoreMoyenMary = (97+134+105)/3;
+
 //    Comme avant, imprimez le gagnant en moyenne dans la console.
 //    INDICE : vous allez avoir besoin de l'opérateur && pour prendre la décision.
+if ((scoreMoyenMary > scoreMoyenJohn) && (scoreMoyenMary > scoreMoyenMike)){
+    console.log(`mary a le  plus haut score moyen ${scoreMoyenMary} > ${scoreMoyenMike} && ${scoreMoyenJohn}`);
+}else {
+    if ((scoreMoyenJohn > scoreMoyenMike) && (scoreMoyenJohn > scoreMoyenMary) ){
+        console.log(`john a le  plus haut score moyen ${scoreMoyenJohn} > ${scoreMoyenMike} && ${scoreMoyenMary}`);
+    }else {
+        console.log(`mike a le  plus haut score moyen ${scoreMoyenMike} > ${scoreMoyenJohn} && ${scoreMoyenMary}`);
+    }
+}
 // 5. Changez là aussi les scores pour générer différents gagnants,
 //    en gardant toujours en tête qu'il pourrait y avoir des ex æquos.
+scoreMoyenJohn = (155+120+103)/3;
+scoreMoyenMike = (116+94+123)/3;
+scoreMoyenMary = (10+134+105)/3;
+
+//    Comme avant, imprimez le gagnant en moyenne dans la console.
+//    INDICE : vous allez avoir besoin de l'opérateur && pour prendre la décision.
+if ((scoreMoyenMary > scoreMoyenJohn) && (scoreMoyenMary > scoreMoyenMike)){
+    console.log(`mary a le  plus haut score moyen ${scoreMoyenMary} > ${scoreMoyenMike} && ${scoreMoyenJohn}`);
+}else {
+    if ((scoreMoyenJohn > scoreMoyenMike) && (scoreMoyenJohn > scoreMoyenMary) ){
+        console.log(`john a le  plus haut score moyen ${scoreMoyenJohn} > ${scoreMoyenMike} && ${scoreMoyenMary}`);
+    }else {
+        console.log(`mike a le  plus haut score moyen ${scoreMoyenMike} > ${scoreMoyenJohn} && ${scoreMoyenMary}`);
+    }
+}
